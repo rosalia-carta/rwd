a1538a96061932644a623bfd3d9685556411b9c1
rosalia-carta <rosalia.carta@gmail.com>
2017-04-10 22:49:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+$(function () {
+    'use strict';
 
-var	lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-lorem= lorem + lorem
-$ ('.lorem1').html (lorem);
-$ ('.lorem2').html (lorem);
-$ ('.lorem3').html (lorem);
+    var Testolorem, lorem1, lorem2, lorem3, lorem4;
+
+    Testolorem = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+    lorem1 =  '<header><h1>Titolo 1</h1></header>' + '<div class="imageborder"><img src="images/rai.jpg"></div>'+ Testolorem   + Testolorem + Testolorem;
+    lorem2 =  '<header><h1>Titolo 2</h1></header>' + Testolorem +  Testolorem +'<div class="imageborder"><img src="images/rai0.jpg"></div>' + Testolorem;
+    lorem3 =  '<header><h1>Titolo 3</h1></header>' + '<div class="imageborder"><img src="images/rai00.jpg"></div>' + Testolorem +  Testolorem + Testolorem;
+    lorem4 =  '<header><h1>Titolo 4</h1></header>' + Testolorem + Testolorem + Testolorem +'<div class="imageborder"><img src="images/rai000.jpg"></div>';
+
+    $(".lorem_testo1").html(lorem1);
+    $(".lorem_testo2").html(lorem2);
+    $(".lorem_testo3").html(lorem3);
+    $(".lorem_testo4").html(lorem4);
+});
